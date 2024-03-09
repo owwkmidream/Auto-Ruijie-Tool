dd296f2e31f2b582b26775a6a99a5a584247009b
owwkmidream <159dreaml@gmail.com>
2024-03-09 15:30:33 +0800

    ✨ 添加新特性: 踢出在线设备

--- main.go
+++ main.go
@@ -57,12 +57,133 @@ func SendRequest(endpoint string, requestData map[string]string) bool {
 	res, _ := result["result"]
 	log.Info("响应：", result)
 	if res == "success" {
-
 		return true
+	} else if msg := result["message"]; msg == "你使用的账号已达到同时在线用户数量上限!" {
+		notify.Send("登录失败", "你使用的账号已达到同时在线用户数量上限!")
+		log.Info("账户上限，执行踢出设备操作")
+		KickDevices()
+		return Login()
 	} else {
 		log.Error("请求体：", resp.Request)
 		return false
 	}
+}
+
+func KickDevices() bool {
+	// 首先获取cookies
+	client := &http.Client{
+		Transport: http.DefaultTransport,
+	}
+
+	req, err := http.NewRequest("GET", config.URL["manage_host"]+config.URL["get_cookie"], nil)
+	if err != nil {
+		log.Error("创建请求时发生错误", err)
+		return false
+	}
+	// 添加ManageHeaders到请求头
+	for key, value := range config.ManageHeaders {
+		req.Header.Add(key, value)
+	}
+
+	// 把ManageParams添加到get请求的URL中
+	query := req.URL.Query()
+	for key, value := range config.ManageParams {
+		query.Add(key, value)
+	}
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Error("发送请求时发生错误", err)
+		return false
+	}
+
+	// 获取cookies
+	cookies := resp.Cookies()
+	defer resp.Body.Close()
+
+	// 使用cookies获取设备列表
+	req, err = http.NewRequest("GET", config.URL["manage_host"]+config.URL["get_devices"], nil)
+	if err != nil {
+		log.Error("创建请求时发生错误", err)
+		return false
+	}
+	for _, cookie := range cookies {
+		req.AddCookie(cookie)
+	}
+
+	//发送请求，解析响应的html
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Error("发送请求时发生错误", err)
+		return false
+	}
+	bodyReader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
+	bodyBytes, err := io.ReadAll(bodyReader)
+	if err != nil {
+		log.Error("读取响应体时发生错误", err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	bodyString := string(bodyBytes)
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(bodyString))
+	if err != nil {
+		log.Error("解析html时发生错误", err)
+		return false
+	}
+
+	// 获取#mydiv > div[id*="divId"]
+	divs := doc.Find("div[id*=\"divId\"]")
+	divs.Each(func(i int, selection *goquery.Selection) {
+		// 得到div的nodeList，获取子元素
+		// #a1得到IP地址
+		ip := selection.Find("#a1").Text()
+		ip = regexp.MustCompile(`\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b`).FindString(ip)
+
+		// label得到设备名称
+		label := selection.Find("label").Text()
+		label = strings.TrimSpace(label)
+
+		log.Info("检测到设备：", label, "ip: ", ip)
+
+		// label不包含"电脑"or"路由器"，则踢出
+		if !strings.Contains(label, "电脑") && !strings.Contains(label, "路由器") && !strings.Contains(label, "校园网") {
+			log.Info("踢出设备：", label, " ip: ", ip)
+			// 踢出设备
+			req, err = http.NewRequest("POST", config.URL["manage_host"]+config.URL["kick"], nil)
+			// 添加kick params
+			query = req.URL.Query()
+			for key, value := range config.KickParams {
+				query.Add(key, value)
+			}
+			req.URL.RawQuery = query.Encode()
+			// 添加body
+			body := strings.NewReader(url.Values{
+				"key": {config.LoginData["userId"] + ":" + ip},
+			}.Encode())
+			req.Body = io.NopCloser(body)
+			req.ContentLength = int64(body.Len())
+
+			req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+			for _, cookie := range cookies {
+				req.AddCookie(cookie)
+			}
+
+			// 发送请求
+			resp, err = client.Do(req)
+			if err != nil {
+				log.Error("发送请求时发生错误", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			log.Infof("踢出设备 %v 成功", label)
+		}
+	})
+
+	return true
 }
 
 func Login() bool {
